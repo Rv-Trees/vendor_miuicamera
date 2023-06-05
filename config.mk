5d321d1d7e3816eadf83a221fa00bc9827e1dd35
resist15 <souravhjagtap21@gmail.com>
2023-06-05 20:44:39 +0530

    MiuiCamera: beryllium edition
    - Modified MiuiCamera For Beryllium Features
    - Restructure Repo For Our Usage
    - Drop extract-utils
    - Update Libs from V12.0.3.0.QEJMIXM
    - Add Notch Props
    - Cleanup Sepolicy

--- config.mk
+++ config.mk
@@ -1,23 +1,26 @@
-# Copyright (C) 2020 Paranoid Android
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/miuicamera
 
-$(call inherit-product, vendor/miuicamera/common/common-vendor.mk)
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/system/priv-app/MiuiCamera/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/system/priv-app/MiuiExtraPhoto/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiExtraPhoto/lib) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/vendor/etc,$(TARGET_COPY_OUT_VENDOR)/etc) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/vendor/lib,$(TARGET_COPY_OUT_VENDOR)/lib) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
+    $(call find-copy-subdir-files,*,vendor/miuicamera/proprietary/vendor/camera,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/camera)
 
-# Notch
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+PRODUCT_PACKAGES += \
+    MiuiCamera \
+    MiuiExtraPhoto
+
+# Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.lens.oem_camera_package=com.android.camera
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.vendor.camera.privapp.list=com.android.camera \
     ro.miui.notch=1
 
-# Sepolicy
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
-    vendor/miuicamera/sepolicy/private
+include vendor/miuicamera/BoardConfig.mk
